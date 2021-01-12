d2444d6d0c21e936c73d4f8d73825064887307c3
everestq <sergey_shelepov@list.ru>
2021-01-12 10:41:18 +0300

    kurs1 completed

--- vue.js
+++ vue.js
@@ -12,28 +12,52 @@ const App = {
         {title: 'Роутер', text: 'В данном блоке вы узнаете все о том, как работает мультиязычность во Vue. Мы создадим миниклон Gmail в данном блоке, где вы на практике увидите как работать с динамическим роутером.'},
         {title: 'Vuex', text: 'В блоке вы узнаете абсолютно все про Vuex. Вы узнаете как работать с данными, какие есть лучшие практики по их программированию и структурированию. Все на практике.'},
         {title: 'Composition', text: 'Одним из наиболее важных обновлений в Vue 3 является появление альтернативного синтаксиса Composition API. В этом блоке вы узнаете все, чтобы полностью пользоваться данными синтаксисом на практических примерах. Помимо этого вы узнаете как работать совместно с Vue Router и Vuex.'},
-      ]
+      ],
+      finish: false
     }
   },
   methods: {
     prev() {
       // когда нажимаем кнопку назад
+      this.activeIndex--
     },
     reset() {
       // начать заново
+      this.activeIndex = 0
+      this.finish = false
     },
     nextOfFinish() {
       // кнопка вперед или закончить
+      if (this.activeIndex < this.steps.length - 1) {
+        this.activeIndex++
+      } else {
+        this.finish = true
+      }
+
     },
     setActive(idx) {
       // когда нажимаем на определенный шаг
+      this.activeIndex = idx
     }
   },
   computed: {
     // тут стоит определить несколько свойств:
     // 1. текущий выбранный шаг
+    currentStep() {
+      return this.steps[this.activeIndex]
+    },
     // 2. выключена ли кнопка назад
+    activePrevBtn() {
+      if (this.activeIndex === 0) {
+        return true
+      }
+    },
     // 3. находимся ли мы на последнем шаге
+    lastStep() {
+      if (this.activeIndex === this.steps.length - 1) {
+        return true
+      }
+    }
   }
 }
 
